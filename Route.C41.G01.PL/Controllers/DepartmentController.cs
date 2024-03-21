8eeaa1a95b28d5bee48ed30994d09bd6656c6825
Omar Wael <waelmero28@gmail.com>
2024-03-21 17:50:03 +0200

    Create Action "Create" for Department Controller Create View "Create" for Department Controller

--- Route.C41.G01.PL/Controllers/DepartmentController.cs
+++ Route.C41.G01.PL/Controllers/DepartmentController.cs
@@ -23,5 +23,26 @@ namespace Route.C41.G01.PL.Controllers
 
             return View(departments);
         }
+
+        // /Department/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if(ModelState.IsValid) // server side validation
+            {
+                var count = _departmentsRebo.Add(department);
+                if(count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
     }
 }

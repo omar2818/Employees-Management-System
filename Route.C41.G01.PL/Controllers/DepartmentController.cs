c21ab5111eb606414167506f7db51da2013755b2
Omar Wael <waelmero28@gmail.com>
2024-03-23 19:17:54 +0200

    Add "AntiForgeryToken" for "Edit" Action in Department Controller

--- Route.C41.G01.PL/Controllers/DepartmentController.cs
+++ Route.C41.G01.PL/Controllers/DepartmentController.cs
@@ -89,6 +89,7 @@ namespace Route.C41.G01.PL.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Edit([FromRoute] int id, Department department)
         {
             if(id != department.Id)

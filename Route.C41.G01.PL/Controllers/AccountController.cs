1f02becb80a4409deb1f989592a7988bb59b5f0d
Omar Wael <waelmero28@gmail.com>
2024-10-01 08:53:11 +0300

    Update External Login using google

--- Route.C41.G01.PL/Controllers/AccountController.cs
+++ Route.C41.G01.PL/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
